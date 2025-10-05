73e327c9de6ca31403b45ccf18ec7df61bea24c8
RoseKikiQ <Rosellucyc@gmail.com>
2025-10-05 12:15:12 -0600

    ending

--- Treasure of Zodiac/SceneTriggerEnding.gd
+++ Treasure of Zodiac/SceneTriggerEnding.gd
@@ -1,4 +1,4 @@
-class_name Scenetrigger extends Area2D
+extends Area2D
 
 @export var required_coins: Array = ["KeyA1", "KeyA2", "KeyA3", "KeyB1", "KeyB2", "KeyB3", 
 									 "KeyC1", "KeyC2", "KeyC3", "KeyD1", "KeyD2", "KeyD3"]
@@ -14,4 +14,3 @@ func _on_body_entered(body: Node2D) -> void:
 			var full_path = scene_folder + connect_scene + ".tscn"
 			var scene_tree = get_tree()
 			scene_tree.call_deferred("change_scene_to_file", full_path)
-		
